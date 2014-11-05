58dccba0316b5e2801e665657a8cb52cda998fdc
Marcellin Nshimiyimana <mars@fusemachines.com>
2014-11-05 01:11:27 -0500

    reached the Security with methods part

--- social-eq.js
+++ social-eq.js
@@ -4,11 +4,58 @@ if (Meteor.isClient) {
   // This code only runs on the client
   Template.body.helpers({
     questions: function () {
-      return Questions.find({});
+
+      if(Session.get("hideCompleted")){ //filter out used questions
+        return Questions.find({ checked: {$ne: true}}, {sort: {createdAt: -1}});
+
+      } else {
+        return Questions.find({}, {sort: {createdAt: -1}});
+
+      }
+
+    },
+    incompleteCount: function(){
+      return Questions.find({checked: {$ne: true}}).count();
+    },
+    hideCompleted: function(){
+      return Session.get("hideCompleted");
     }
+  });
+
+  Template.body.events({
+    //this function is called when the new task form is submitted
+    "submit .new-question": function(event){
+
+      var question = event.target.question.value;
+
+      Meteor.call("addQuestion", question);
+      //clear form here
+      event.target.question.value = "";
+
+      //prevent default form submit
+      return false;
+    },
+
+    "click .toggle-checked": function(){
+      //set the checked property to the opposite of its current value
+      Meteor.call("setChecked", this._id, ! this.checked);
+
+    },
+    "click .delete": function(){
+      Meteor.call("deleteQuestion", this._id);
+
+    },
+    // handle hideCompleted event
+    "change .hide-completed input": function(event){
+      Session.set("hideCompleted", event.target.checked);
+    }
+
   });
 }
 
+// Accounts.ui.config({
+//   passwordSignupFields: "USERNAME_ONLY"
+// });
 
 Meteor.methods({
   addQuestion: function (question) {
@@ -17,7 +64,7 @@ Meteor.methods({
       throw new Meteor.Error("not-authorized");
     }
 
-    Tasks.insert({
+    Questions.insert({
       question: question,
       createdAt: new Date(),
       owner: Meteor.userId(),
